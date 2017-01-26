4e34e6d8c00f54f7dcd5c25ae036534fbea07634
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-01-26 16:10:21 -0500

    Added TraceTimer

--- samples/TestZip/Source/MainOpenFL.hx
+++ samples/TestZip/Source/MainOpenFL.hx
@@ -7,16 +7,12 @@ import openfl.display.Sprite;
  */
 class MainOpenFL extends Sprite
 {
-  var stats:Stats = new Stats();
   var test:TestZip;
 
   // Run some tests
 	public function new()
   {
 		super();
-
-    // Stats
-    addChild(stats);
 
     // Test
 		test = new TestZip();
--- samples/TestZip/Source/TestZip.hx
+++ samples/TestZip/Source/TestZip.hx
@@ -2,6 +2,8 @@ package;
 
 import haxe.ds.StringMap;
 import multiloader.MultiLoader;
+import statistics.Stats;
+import trace.TraceTimer;
 
 import zip.Zip;
 import zip.ZipEntry;
@@ -21,6 +23,9 @@ enum Tests
  */
 class TestZip
 {
+  // Stats
+  var stats:Stats = new Stats();
+
   // List of files
   public static inline var PATH:String = "./assets/";
   public static inline var TEST1:String = PATH + "test1.zip";
@@ -28,6 +33,8 @@ class TestZip
   // Run some tests
   public function new()
   {
+    TraceTimer.activate();
+
     trace("TestZip Launch");
 
     var test = LoadURL1;
@@ -42,33 +49,33 @@ class TestZip
   function loadURL1()
   {
     MultiLoader.loadBytes(
-    { 
-      url: TEST1, 
+    {
+      url: TEST1,
       complete: function(bytes)
       {
         trace("Downloading complete");
-        
+
         var entries = new StringMap<ZipEntry>();
-        
+
         var zip = new Zip(bytes);
         var entry:ZipEntry;
-        
+
         // Read your Entry info (you could progressively read your zip entries X number per frame)
         while ( (entry = zip.readEntryHeader()) != null )
         {
           // Do something with your entry
           entries.set(entry.fileName, entry);
-          
+
           trace("Entry", entry.fileName);
         }
-        
+
         // You could get a percentage of task done using this
         var percent = (entry == null) ? 1 : (zip.i.position / zip.i.length);
-        
+
         // Read your entry, will read the Data on request (support Deflate even on JS target)
         var myBytes = Zip.getBytes(entries.get("sheets.xml"));
         var myText = Zip.getString(entries.get("sheets/159288dc66df51f.xml"));
-        
+
         trace("Bytes", myBytes.length);
         trace("Text", myText);
       },
