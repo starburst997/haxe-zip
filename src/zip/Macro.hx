ee07a28ff42816fb510b383fe3497da394b5286e
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-01-30 15:01:44 -0500

    Remove unused file

--- src/zip/Macro.hx
+++ src/zip/Macro.hx
@@ -9,16 +9,15 @@ class Macro
   public static macro function init():Void
   {
     #if !display
-
+    
     // Include pako.js (only if we're not using "openfl", since it already included!)
     if ( haxe.macro.Context.defined("js") && !haxe.macro.Context.defined("openfl") )
     {
       //trace("Included file pako");
-
-      //haxe.macro.Compiler.includeFile("../../externs/pako_deflate.min.js");
+      
       haxe.macro.Compiler.includeFile("../../externs/pako.min.js");
     }
-
+    
     #else
     // Do nothing
     #end
